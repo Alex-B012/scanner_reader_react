06bf335b787502767ce41ed7e03ec29ce211d1b0
Alex-B012 <rad15on2014@gmail.com>
2026-01-29 01:00:32 +0300

    testing mode - draft 1 v1.0.2

--- scanner-reader/src/App.js
+++ scanner-reader/src/App.js
@@ -82,7 +82,7 @@ function App() {
 
       aspectRatio: window.innerWidth / window.innerHeight,
       rememberLastUsedCamera: true,
-      formatsToSupport,
+      formatsToSupport: formatsToSupport,
       // videoConstraints: {
       //   facingMode: "environment",
       // },
